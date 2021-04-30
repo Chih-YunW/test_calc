e89913a71d0797da1339deb0970ecbdda4f007d7
Chih Yun <wuchihy@oregonstate.edu>
2021-04-30 16:05:13 -0700

    add unittest for calc.py

---
+++ test_calc.py
@@ -0,0 +1,11 @@
+import unittest
+import calc
+
+class testCaseAdd(unittest.TestCase):
+	def test_add(self):
+		self.assertEqual(calc.add(1,3), 4)
+	def
+
+
+if __name__ == '__main__':
+	unittest.main()
