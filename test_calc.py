db3e7349a08828d378e73e6d7f69983206ec7417
Chih Yun <wuchihy@oregonstate.edu>
2021-04-30 16:18:41 -0700

    test cases for add

--- test_calc.py
+++ test_calc.py
@@ -4,8 +4,10 @@ import calc
 class testCaseAdd(unittest.TestCase):
 	def test_add(self):
 		self.assertEqual(calc.add(1,3), 4)
-	def
-
+	def test_edge_case_add(self):
+                self.assertEqual(calc.add(0,0), 0)
+	def test_type_add(self):
+		self.assertRaises(TypeError, calc, 'dog', 2)
 
 if __name__ == '__main__':
 	unittest.main()
